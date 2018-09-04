2fbeae5c095c9c16879a85ca1fce1b3c58870ad2
Silver Laius <silver.laius@gmail.com>
2018-09-05 01:01:58 +0300

    added connection to database

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,41 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const mySql = require('mysql')
 const app = express()
+const SELECT_ALL_QUERY = 'SELECT * FROM test'
+
 app.use(bodyParser.json())
+app.use(cors())
+
+const connection = mySql.createConnection({
+	host:'localhost',
+	user:'root',
+	password:'password',
+	database:'kertupertu'
+})
+
+connection.connect(err => {
+	if (err) throw err;
+  	console.log("Connected!");
+})
 
 app.get('/', (req,res) => {
-	res.send('welcome to the home page')
+	res.send('go to /products')
+})
+
+app.get('/products', (req,res) => {
+	connection.query(SELECT_ALL_QUERY, (err, results) => {
+		if(err) {
+			return res.send(err)
+		} 
+		else {
+			return res.json({
+				data: results
+			})
+		}
+
+	})
 })
 
 app.listen(3001, ()=> {
